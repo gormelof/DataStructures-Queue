59f3ba9d421c5a469bae9765b63058a0781e17b1
gormelof <gormelomerfaruk@gmail.com>
2014-11-02 17:45:08 +0200

    Functionality introduced to the basic functions

--- Data Structers - Queue/main.cpp
+++ Data Structers - Queue/main.cpp
@@ -10,24 +10,55 @@
 
 using namespace std;
 
+int queue[5]; // Create queue
+int last, maxLength; // last, queue end. maxLength, max size queue.
+
 bool full () {
-    
+    if (last == maxLength) { // full
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 bool empty () {
-    
+    if (last == 0) {
+        return true; // empty
+    }
+    else {
+        return false;
+    }
 }
 
 void enqueue () {
-    
+    if (!full()) {
+        cout << "Value : ";
+        cin >> queue[last];
+        last++;
+    }
+    else {
+        cout << "Full...\n";
+    }
 }
 
 void dequeue () {
-    
+    if (!empty()) {
+        last--;
+        for (int i = 0; i < last; i++) { // shifting
+            queue[i] = queue [i+1];
+        }
+        queue[last] = 0;
+    }
+    else {
+        cout << "Empty...\n";
+    }
 }
 
 void showQueue () {
-    
+    for (int i = 0; i < maxLength; i++) {
+        cout << "queue[" << i << "] : " << queue[i] << endl;
+    }
 }
 
 int main(int argc, const char * argv[]) {
