731d9d1210055b34c46d41d04396a61459cce133
gormelof <gormelomerfaruk@gmail.com>
2014-11-02 17:39:08 +0200

    Data structures - queue project created
    Created queue project and basic functions defining

---
+++ Data Structers - Queue/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  Data Structers - Queue
+//
+//  Created by Ömer Faruk Görmel on 02/11/14.
+//  Copyright (c) 2014 gormelof. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+bool full () {
+    
+}
+
+bool empty () {
+    
+}
+
+void enqueue () {
+    
+}
+
+void dequeue () {
+    
+}
+
+void showQueue () {
+    
+}
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
